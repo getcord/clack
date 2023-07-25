74053b375d18632afdcc065c488e88e5dd49f3e1
Elena Pérez Rioja <elena@cord.com>
2023-07-25 08:59:36 +0200

    Watching

--- esbuild.config.js
+++
@@ -1,9 +0,0 @@
-const res = require('esbuild').buildSync({
-  entryPoints: ['src/app.tsx'],
-  bundle: true,
-  minify: true,
-  format: 'cjs',
-  sourcemap: true,
-  outfile: 'dist/output.js',
-  // external: ['react', 'react-dom'], 
-})
