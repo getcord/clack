e3313fe0a522bd5f2dd1d447c85a103e57ca3dfb
Josh Watzman <jwatzman@cord.com>
2023-07-25 13:35:24 +0100

    generate-localhost-cert works better on first run

--- scripts/generate-localhost-cert.sh
+++ scripts/generate-localhost-cert.sh
@@ -4,7 +4,9 @@ brew install mkcert
 mkcert -install
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+CERTPATH="$SCRIPTPATH/../dist/localhost"
+mkdir -p  "$CERTPATH"
 mkcert \
-  -cert-file $SCRIPTPATH/../dist/localhost/localhost.crt \
-  -key-file $SCRIPTPATH/../dist/localhost/localhost.key \
+  -cert-file "$CERTPATH"/localhost.crt \
+  -key-file "$CERTPATH"/localhost.key \
   localhost 127.0.0.1 ::1 0.0.0.0
