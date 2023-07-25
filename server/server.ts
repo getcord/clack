9b06280edd6bb55c60f4b5d7efc8716b81784de6
Gillian Yeomans <gillian@cord.com>
2023-07-25 13:34:22 +0100

    add type for users

--- server/server.ts
+++ server/server.ts
@@ -29,20 +29,40 @@ function main() {
     return next();
   });
 
-  const users = {
-    "ernest": {"name":"Ernest","email":"ernest@example.com","profilePictureURL":"https://app.cord.com/static/Anon-avatar-0.png"},
-    "sylvia":{"name":"Sylvia","email":"sylvia@example.com","profilePictureURL":"https://app.cord.com/static/Anon-avatar-1.png"},
-    "oscar":{"name":"Oscar","email":"oscar@example.com","profilePictureURL":"https://app.cord.com/static/Anon-avatar-2.png"}
-}
+  type Users = {
+    [cordUserID: string]: {
+      name: string;
+      email: string;
+      profilePictureURL?: string;
+      slackID?: string;
+    };
+  };
 
-let loginAsUserIndex = 0;
+  const users: Users = {
+    ernest: {
+      name: 'Ernest',
+      email: 'ernest@example.com',
+      profilePictureURL: 'https://app.cord.com/static/Anon-avatar-0.png',
+    },
+    sylvia: {
+      name: 'Sylvia',
+      email: 'sylvia@example.com',
+      profilePictureURL: 'https://app.cord.com/static/Anon-avatar-1.png',
+    },
+    oscar: {
+      name: 'Oscar',
+      email: 'oscar@example.com',
+      profilePictureURL: 'https://app.cord.com/static/Anon-avatar-2.png',
+    },
+  };
 
-const ORG_ID = 'cord_org'
-const ORG_NAME = 'Cord Internal'
+  let loginAsUserIndex = 0;
+
+  const ORG_ID = 'cord_org';
+  const ORG_NAME = 'Cord Internal';
 
   // Supply client token for front-end Cord UI
   app.get('/userToken', (_, res) => {
-
     const userID = Object.keys(users)[loginAsUserIndex];
     loginAsUserIndex =
       loginAsUserIndex + 1 >= Object.keys(users).length
@@ -50,22 +70,20 @@ const ORG_NAME = 'Cord Internal'
         : loginAsUserIndex + 1;
 
     const token = getClientAuthToken(CORD_APP_ID, CORD_SIGNING_SECRET, {
-        user_id: userID,
-        organization_id: ORG_ID,
-        user_details: {
-          email: users[userID].email,
-          name: users[userID].name,
-          profilePictureURL: users[userID].profilePictureURL,
-        },
-        organization_details: {
-          name: ORG_NAME,
-        },
-      })
-
+      user_id: userID,
+      organization_id: ORG_ID,
+      user_details: {
+        email: users[userID].email,
+        name: users[userID].name,
+        profilePictureURL: users[userID].profilePictureURL,
+      },
+      organization_details: {
+        name: ORG_NAME,
+      },
+    });
 
     res.send(token);
   });
-
 
   // Catch errors and log them
   app.use(
