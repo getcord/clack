44541151178a6c30f26685ac9ba2a7a747029736
Gillian Yeomans <gillian@cord.com>
2023-07-25 13:32:38 +0100

    make server work and stuff

--- scripts/generate-localhost-cert.sh
+++ scripts/generate-localhost-cert.sh
@@ -5,6 +5,6 @@ mkcert -install
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 mkcert \
-  -cert-file $SCRIPTPATH/../localhost/localhost.crt \
-  -key-file $SCRIPTPATH/../localhost/localhost.key \
+  -cert-file $SCRIPTPATH/../dist/localhost/localhost.crt \
+  -key-file $SCRIPTPATH/../dist/localhost/localhost.key \
   localhost 127.0.0.1 ::1 0.0.0.0
