7e53f8185a8c13a58f195947ded97ae0c2c6aeb2
Josh Watzman <jwatzman@cord.com>
2023-07-25 14:52:40 +0100

    Split out server handlers

---
+++ src/server/handlers/getChannels.ts
@@ -0,0 +1,5 @@
+import type { Request, Response } from 'express';
+
+export function handleGetChannels(_: Request, res: Response) {
+  res.send(['general', 'what-the-quack', 'e', 'random']);
+}
--- src/server/server.ts
+++ src/server/server.ts
@@ -5,17 +5,11 @@ import cors from 'cors';
 import path from 'path';
 import fs from 'fs';
 import * as dotenv from 'dotenv';
-import { getClientAuthToken } from '@cord-sdk/server';
+import { handleGetToken } from './handlers/getToken';
+import { handleGetChannels } from './handlers/getChannels';
 
 const REPO_ROOT = path.join(__dirname, '..', '..');
 dotenv.config({ path: path.join(REPO_ROOT, '.env') });
-
-export const CORD_APP_ID = process.env.CORD_APP_ID!;
-export const CORD_SIGNING_SECRET = process.env.CORD_SIGNING_SECRET!;
-
-if (!CORD_APP_ID || !CORD_SIGNING_SECRET) {
-  throw new Error('Missing Cord App ID or Signing Secret from env vars');
-}
 
 const port = 3002; // browsersync currently on 3001
 export const FRONT_END_HOST = 'http://localhost:3000';
@@ -30,65 +24,8 @@ function main() {
     return next();
   });
 
-  type Users = {
-    [cordUserID: string]: {
-      name: string;
-      email: string;
-      profilePictureURL?: string;
-      slackID?: string;
-    };
-  };
-
-  const users: Users = {
-    ernest: {
-      name: 'Ernest',
-      email: 'ernest@example.com',
-      profilePictureURL: 'https://app.cord.com/static/Anon-avatar-0.png',
-    },
-    sylvia: {
-      name: 'Sylvia',
-      email: 'sylvia@example.com',
-      profilePictureURL: 'https://app.cord.com/static/Anon-avatar-1.png',
-    },
-    oscar: {
-      name: 'Oscar',
-      email: 'oscar@example.com',
-      profilePictureURL: 'https://app.cord.com/static/Anon-avatar-2.png',
-    },
-  };
-
-  let loginAsUserIndex = 0;
-
-  const ORG_ID = 'cord_org';
-  const ORG_NAME = 'Cord Internal';
-
-  // Supply client token for front-end Cord UI
-  app.get('/token', (_, res) => {
-    const userID = Object.keys(users)[loginAsUserIndex];
-    loginAsUserIndex =
-      loginAsUserIndex + 1 >= Object.keys(users).length
-        ? 0
-        : loginAsUserIndex + 1;
-
-    const token = getClientAuthToken(CORD_APP_ID, CORD_SIGNING_SECRET, {
-      user_id: userID,
-      organization_id: ORG_ID,
-      user_details: {
-        email: users[userID].email,
-        name: users[userID].name,
-        profilePictureURL: users[userID].profilePictureURL,
-      },
-      organization_details: {
-        name: ORG_NAME,
-      },
-    });
-
-    res.send({ token, userID });
-  });
-
-  app.get('/channels', (_, res) => {
-    res.send(['general', 'what-the-quack', 'e', 'random']);
-  });
+  app.get('/token', handleGetToken);
+  app.get('/channels', handleGetChannels);
 
   // Catch errors and log them
   app.use(
