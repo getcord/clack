6c0a457811805456aaff9c872048239ef3b3daef
Gillian Yeomans <gillian@cord.com>
2023-07-31 17:13:52 +0100

    move client files around

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -10,6 +10,9 @@ tsc --watch --preserveWatchOutput &
 npm run build-server &
 npm run build-client &
 
+cp -r ./src/client/static ./dist/client/static & 
+cp -r ./src/client/index.html ./dist/client/index.html & 
+
 npm run run-server &
 npm run run-client &
 
