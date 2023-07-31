1ab8e1b1b7d8088dd26c333fefd9b61ee92a05a3
Khadija Nur <khadija@Khadijas-MacBook-Pro.local>
2023-07-31 12:58:42 +0100

    Add preview facepile to users in channel and update tooltip styling

--- src/client/PageUsersLabel.tsx
+++ src/client/PageUsersLabel.tsx
@@ -3,17 +3,39 @@ import { UserIcon } from '@heroicons/react/24/outline';
 import { Tooltip } from 'react-tooltip';
 import styled from 'styled-components';
 import { Colors } from 'src/client/Colors';
+import { Facepile, user } from '@cord-sdk/react';
 
 export function PageUsersLabel({ users }: { users: (string | number)[] }) {
+  // skip ernest the bot
+  const previewUsers = users.slice(1, 4) as string[];
+  const userData = previewUsers.map((id) => user.useUserData(id));
+
   return (
     <>
-      <Tooltip id="page-users-label" />
+      <Tooltip
+        id="page-users-label"
+        style={{
+          borderRadius: '8px',
+          backgroundColor: '#000',
+        }}
+      >
+        <TooltipText>
+          <span>View all members of this channel.</span>
+          <span>{`Includes ${userData
+            .map((user) => user?.name ?? '')
+            .join(', ')}`}</span>
+        </TooltipText>
+      </Tooltip>
       <UsersLabel
         data-tooltip-id="page-users-label"
-        data-tooltip-content="View all members of this channel"
         data-tooltip-place="bottom"
+        aria-multiline={true}
       >
-        <UserIcon width={18} height={18} />
+        {/* notes: type of users coming from cord 
+        api not matching what is expected by cord component
+         */}
+        <StyledFacepile users={previewUsers} enableTooltip={false} />
+        <StyledUserIcon width={18} height={18} />
         <UserCount>{users.length}</UserCount>
       </UsersLabel>
     </>
@@ -23,12 +45,33 @@ export const UsersLabel = styled.div`
   display: flex;
   margin: 11px;
   padding: 8px 12px;
-  gap: 4px;
+  gap: 8px;
   font-size: 13px;
   border-radius: 4px;
   &:hover {
     background-color: ${Colors.gray_highlight};
   }
+`;
+
+export const StyledUserIcon = styled(UserIcon)`
+  @media (min-width: 600px) {
+    display: none;
+  }
+`;
+
+export const StyledFacepile = styled(Facepile)`
+  @media only screen and (min-width: 800) {
+    display: none;
+  }
+`;
+
+export const TooltipText = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  max-width: 200px;
+  padding: 8px 12px;
+  margin: 0;
 `;
 
 export const UserCount = styled.span`
