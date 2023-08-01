2fb1b890a1dee03aab73c322c761a2ddd7c2c7f5
Josh Watzman <jwatzman@cord.com>
2023-08-01 10:46:00 +0100

    Fix channel sync listing empty channel

--- scripts/sync-channels.sh
+++ scripts/sync-channels.sh
@@ -7,6 +7,6 @@ source .env
 TOKEN=$(jwt encode -A HS512 -e=$(($(date +%s) + 30)) --secret "$CORD_SIGNING_SECRET" -P app_id="$CORD_APP_ID")
 
 CHANNEL_FILE="$(dirname "$0")/../channels.txt"
-CHANNELS=$(cat "$CHANNEL_FILE" | tr '\n' '#')
+CHANNELS=$(cat "$CHANNEL_FILE" | perl -pe 'chomp if eof' | tr '\n' '#')
 curl --oauth2-bearer "$TOKEN" https://api.staging.cord.com/v1/users/all_channels_holder -X PUT --json "{\"metadata\":{\"channels\":\"$CHANNELS\"}}"
 echo
