30a629d38c67ed75875f7963463f5a58a86faaf0
Josh Watzman <jwatzman@cord.com>
2023-08-02 14:49:31 +0100

    Tweak dev and prod scripts

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -7,13 +7,17 @@ set -x
 
 tsc --watch --preserveWatchOutput &
 
-npm run build-server &
-npm run build-client &
+# Do a single build first, synchronously, so that we know something exists
+# before trying to launch.
+npm run build-server
+npm run build-client
+npm run build-server -- --watch=forever &
+npm run build-client -- --watch=forever &
 
 cp -r ./src/client/static ./dist/client/static & 
 cp -r ./src/client/index.html ./dist/client/index.html & 
 
-npm run run-server &
-npm run run-client &
+npm run run-server-dev &
+npm run run-client-dev &
 
 wait
--- scripts/prod.sh
+++ scripts/prod.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-trap 'kill $(jobs -p)' EXIT
-
 set -e
 set -x
 
-npm run build-server &
-npm run build-client &
+npm run build-server
+npm run build-client
 
-cp -r ./src/client/static ./dist/client/static & 
-cp -r ./src/client/index.html ./dist/client/index.html & 
+cp -r ./src/client/static ./dist/client/static
+cp -r ./src/client/index.html ./dist/client/index.html
 
-sudo cp -r /home/ec2-user/clack/dist/client /var/www/clack &
+sudo cp -r /home/ec2-user/clack/dist/client /var/www/clack
 
-npm run run-server &
 sudo systemctl reload nginx
-
-wait
+npm run run-server
