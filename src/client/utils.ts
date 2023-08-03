242247e1dc0d5c29ebd49ea8be87a0c45c81d3d3
Khadija Nur <khadija@Khadijas-MacBook-Pro.local>
2023-08-03 10:44:44 +0100

    Create util function for joining strings in array

--- src/client/utils.ts
+++ src/client/utils.ts
@@ -4,3 +4,17 @@ export function capitalize(string: string): string {
     .map((word) => `${word[0].toUpperCase()}${word.slice(1)}`)
     .join(' ');
 }
+
+export function combine(combiner: string, items: string[]): string {
+  if (items.length === 0) {
+    return '';
+  } else if (items.length === 1) {
+    return items[0];
+  } else if (items.length === 2) {
+    return `${items[0]} ${combiner} ${items[1]}`;
+  } else {
+    return `${items.slice(0, -1).join(', ')} ${combiner} ${
+      items[items.length - 1]
+    }`;
+  }
+}
