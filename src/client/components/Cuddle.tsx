24bf03695bcd04c7750d200385f82d61e7aa7e41
Ludo <ludo@cord.com>
2023-08-22 13:34:04 +0100

    hide lk chat

--- src/client/components/Cuddle.tsx
+++ src/client/components/Cuddle.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import { LiveKitRoom, VideoConference } from '@livekit/components-react';
 import '@livekit/components-styles';
+import styled from 'styled-components';
 
 const wsURL = 'wss://cuddle-test-yjaxne2q.livekit.cloud';
 
 export default function Cuddle({ token }: { token?: string }) {
-  console.log({ token });
   return (
     <div data-lk-theme="default">
-      <LiveKitRoom
+      <StyledLiveKitRoom
         token={token}
         serverUrl={wsURL}
         connect={true}
         data-lk-theme="default"
       >
         <VideoConference />
-      </LiveKitRoom>
+      </StyledLiveKitRoom>
     </div>
   );
 }
+
+const StyledLiveKitRoom = styled(LiveKitRoom)`
+  .lk-chat, .lk-button.lk-chat-toggle {
+    display: none;
+  }
+`;
