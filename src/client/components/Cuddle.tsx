d16de66de0c7152f64b7a8737641701149440fdd
Ludo <ludo@cord.com>
2023-08-23 08:14:47 +0100

    Do forbidden things: create the cuddle in a new window and forget about it.

--- src/client/components/Cuddle.tsx
+++ src/client/components/Cuddle.tsx
@@ -1,12 +1,19 @@
-import React, { useCallback, useEffect } from 'react';
+import React, {
+  PropsWithChildren,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import {
-  useDisconnectButton,
   useParticipants,
   LiveKitRoom,
   VideoConference,
 } from '@livekit/components-react';
 import '@livekit/components-styles';
 import styled from 'styled-components';
+import { createPortal } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 const serverUrl = 'wss://cuddle-test-yjaxne2q.livekit.cloud';
 
@@ -21,18 +28,20 @@ export default function Cuddle({
     onQuit();
   }, [onQuit]);
   return (
-    <div data-lk-theme="default">
-      <StyledLiveKitRoom
-        token={token}
-        serverUrl={serverUrl}
-        connect={true}
-        data-lk-theme="default"
-        onDisconnected={onDisconnected}
-      >
-        <VideoConference />
-        <Trying />
-      </StyledLiveKitRoom>
-    </div>
+    <RenderInPersistentWindow>
+      <div data-lk-theme="default">
+        <StyledLiveKitRoom
+          token={token}
+          serverUrl={serverUrl}
+          connect={true}
+          data-lk-theme="default"
+          onDisconnected={onDisconnected}
+        >
+          <VideoConference />
+          <Trying />
+        </StyledLiveKitRoom>
+      </div>
+    </RenderInPersistentWindow>
   );
 }
 
@@ -49,3 +58,116 @@ const StyledLiveKitRoom = styled(LiveKitRoom)`
     display: none;
   }
 `;
+
+const RenderInWindow = (
+  props: PropsWithChildren<{ dangerouslyKeepWindowOnUnmount?: boolean }>,
+) => {
+  const [container, setContainer] = useState<HTMLDivElement | null>(null);
+  const newWindow = useRef<Window | null>(null);
+  const keepWindowOnUnmountRef = useRef(props.dangerouslyKeepWindowOnUnmount);
+  useEffect(() => {
+    keepWindowOnUnmountRef.current = props.dangerouslyKeepWindowOnUnmount;
+  }, [props.dangerouslyKeepWindowOnUnmount]);
+
+  useEffect(() => {
+    // Create container element on client-side
+    setContainer(document.createElement('div'));
+  }, []);
+
+  useEffect(() => {
+    // When container is ready
+    if (container) {
+      // Create window
+      newWindow.current = window.open(
+        '',
+        '',
+        'width=600,height=400,left=200,top=200',
+      );
+      if (!newWindow.current) {
+        return;
+      }
+      // Append container
+
+      newWindow.current.document.body.appendChild(container);
+
+      copyStyles(document, newWindow.current.document);
+      // Save reference to window for cleanup
+      const curWindow = newWindow.current;
+
+      // Return cleanup function
+      return () => {
+        if (keepWindowOnUnmountRef.current) {
+          return;
+        }
+        curWindow.close();
+      };
+    }
+  }, [container]);
+
+  return (
+    container && createPortal(React.cloneElement(props.children), container)
+  );
+};
+
+// What have I done.
+// This creates and forget a new window and a react app in there.
+const RenderInPersistentWindow = (props: PropsWithChildren) => {
+  const [container, setContainer] = useState<HTMLDivElement | null>(null);
+  const newWindow = useRef<Window | null>(null);
+
+  useEffect(() => {
+    // Create container element on client-side
+    setContainer(document.createElement('div'));
+  }, []);
+
+  useEffect(() => {
+    // When container is ready
+    if (container) {
+      // Create window
+      newWindow.current = window.open(
+        '',
+        '',
+        'width=600,height=400,left=200,top=200',
+      );
+      if (!newWindow.current) {
+        return;
+      }
+      // Append container
+
+      newWindow.current.document.body.appendChild(container);
+
+      copyStyles(document, newWindow.current.document);
+      // Save reference to window for cleanup
+      const curWindow = newWindow.current;
+      const root = createRoot(container);
+      root.render(props.children);
+
+      // Return cleanup function
+      return () => {
+        return;
+        // curWindow.close();
+      };
+    }
+    // I miss a dep because do not want to recreate a react root when the child change.
+    // There is some cleaner way to do that.
+  }, [container]);
+  return null;
+};
+
+function copyStyles(src: Document, dest: Document) {
+  Array.from(src.styleSheets).forEach((styleSheet) => {
+    const styleElement = styleSheet.ownerNode?.cloneNode(true) as
+      | StyleSheet['ownerNode']
+      | undefined;
+    if (!styleElement) {
+      return;
+    }
+    styleElement.href = styleSheet.href;
+    dest.head.appendChild(styleElement);
+  });
+  try {
+    Array.from(src.fonts).forEach((font) => dest.fonts.add(font));
+  } catch (e) {
+    console.warn('error copying fonts', e);
+  }
+}
