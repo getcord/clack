826fa35d173639b2dd539c01fec8d6624cf3b858
Josh Watzman <jwatzman@cord.com>
2023-10-24 16:39:48 +0100

    [𝘀𝗽𝗿] changes to main this commit is based on
    Created using spr 1.3.5-beta.1
    
    [skip ci]

--- src/server/handlers/getUsersInOrg.ts
+++ src/server/handlers/getUsersInOrg.ts
@@ -34,6 +34,18 @@ export async function handleGetCordUsersData(_: Request, res: Response) {
 }
 
 export async function handleUpdateUserStatus(req: Request, res: Response) {
+  const { user_id } = (req as any).loginTokenData;
+  if (user_id !== req.params.userID) {
+    res.status(401).send('Cannot set status for another user');
+    return;
+  }
+
+  const keys = Object.keys(req.body);
+  if (keys.length !== 1 || keys[0] !== 'metadata') {
+    res.status(403).send('Can only set metadata');
+    return;
+  }
+
   const user = await fetchCordRESTApi<ServerUpdateUser>(
     `users/${req.params.userID}`,
     'PUT',
