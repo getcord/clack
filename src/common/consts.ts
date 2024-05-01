32f567df311c00b27ce091dbfe7372eb6a0421cb
Adam Vartanian <flooey@cord.com>
2024-05-01 15:31:22 +0100

    Add extractUsersFromDirectMessageChannel
    This code is repeated in a few places (and I'm about to add a couple
    more), so make a helper function for it.

--- src/common/consts.ts
+++ src/common/consts.ts
@@ -4,3 +4,10 @@ export const EVERYONE_ORG_ID = 'clack_all';
 export function isDirectMessageChannel(id: string) {
   return id.startsWith(DM_CHANNEL_PREFIX);
 }
+
+export function extractUsersFromDirectMessageChannel(id: string) {
+  if (!isDirectMessageChannel(id)) {
+    throw new Error('Only call this function with direct message channels');
+  }
+  return id.substring(DM_CHANNEL_PREFIX.length).split(',').sort();
+}
